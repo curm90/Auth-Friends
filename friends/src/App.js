505f6a690e0fb26633002fa141abdfdd868aacdd
Liam Sutton <devliam90@gmail.com>
2019-10-15 12:38:10 +0100

    wrap app in router and build basic form and form route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+const LoginForm = () => {
+  return (
+    <div>
+      <form>
+        <input type='text' placeholder='Username' name='username' />
+        <input type='password' placeholder='Password' name='password' />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello World!</h1>
+      <Route exact path='/' component={LoginForm} />
     </div>
   );
 }
