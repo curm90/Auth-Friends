eb8d1ff1448d0d412e9c2d6774f9c06cc822ae44
Liam Sutton <devliam90@gmail.com>
2019-10-16 17:08:36 +0100

    test

--- friends/src/App.js
+++ friends/src/App.js
@@ -11,6 +11,7 @@ import Navigation from './components/Navigation';
 function App() {
   return (
     <div className='App'>
+      hello world
       <Navigation />
       <Route exact path='/' component={LoginForm} />
       <PrivateRoute path='/friends' component={FriendsList} />
